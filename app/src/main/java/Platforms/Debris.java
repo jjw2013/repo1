6fac9eae0c3c8ed0f10110a124a38ec26659977d
jjw <pinkjery3@gmail.com>
2025-05-10 21:16:57 +0900

    커밋 안한 사항 추가

--- app/src/main/java/Platforms/Debris.java
+++ app/src/main/java/Platforms/Debris.java
@@ -1,4 +1,58 @@
 package Platforms;
 
-public class Debris {
+import android.graphics.Canvas;
+import android.graphics.RectF;
+
+import com.example.doodle.Camera;
+import com.example.doodle.MainScene;
+
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.interfaces.IGameObject;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.interfaces.ILayerProvider;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.interfaces.IRecyclable;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.Sprite;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.Metrics;
+
+public abstract class Debris extends Sprite implements IRecyclable, ILayerProvider {
+
+
+    public Debris (int mipmapId) {
+        super(mipmapId);
+    }
+    public abstract Debris init(float x , float y);
+
+
+    @Override
+    public void draw(Canvas canvas) {
+
+        super.setDstRectWithCamera(Camera.getCameraY(y));
+        super.draw(canvas);
+        super.revertDstRect();
+    }
+
+    @Override
+    public void update() {
+        super.update();
+
+        if(isOutOfCameraRange())
+            Scene.top().remove(this);
+
+    }
+
+    private boolean isOutOfCameraRange(){
+
+        if (this.y < Camera.worldY - Metrics.height/2 ) return true;
+
+        return false;
+
+    }
+
+    public MainScene.Layer getLayer() {
+        return MainScene.Layer.debris;
+    }
+
+    @Override
+    public void onRecycle() {
+
+    }
 }
--- app/src/main/java/com/example/doodle/Background.java
+++ app/src/main/java/com/example/doodle/Background.java
@@ -1,4 +1,32 @@
 package com.example.doodle;
 
-public class Background {
+import android.graphics.Canvas;
+
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.interfaces.IGameObject;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.interfaces.ILayerProvider;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.Sprite;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.Metrics;
+
+public class Background extends Sprite implements ILayerProvider, IGameObject {
+
+    private static final float PLANE_WIDTH = Metrics.width;
+    private static final float PLANE_HEIGHT = PLANE_WIDTH * 368 / 337;
+
+
+    public Background(){
+        super(R.mipmap.note_background);
+        setPosition(Metrics.width/2,Metrics.height/2,PLANE_WIDTH, PLANE_HEIGHT);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.setDstRectWithCamera(Camera.getCameraY(y));
+        super.draw(canvas);
+        super.revertDstRect();
+    }
+
+    @Override
+    public Enum getLayer() {
+        return MainScene.Layer.bg;
+    }
 }
