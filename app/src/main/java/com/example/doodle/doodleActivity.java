c0517b391e7e96537741d5bc82fca1e073c90e62
jjw1212-admin <pinkjery3@gmail.com>
2025-06-13 20:53:21 +0900

    다시 doodleactivity가 tiltevent를 받게함

--- app/src/main/java/com/example/doodle/doodleActivity.java
+++ app/src/main/java/com/example/doodle/doodleActivity.java
@@ -1,6 +1,11 @@
 package com.example.doodle;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +14,55 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.activity.GameActivity;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
 
-public class doodleActivity extends GameActivity {
+public class doodleActivity extends GameActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         new MenuScene().push();
-
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (accelerometer != null) {
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+        }
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float tiltX = event.values[0];
+            Log.d("SensorTest", "onSensorChanged called, tiltX = " + tiltX);
+
+            Scene scene = Scene.top();
+            if (scene instanceof MainScene) {
+                ((MainScene) scene).onTiltEvent(tiltX);
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // 필요시 구현
+    }
 }
+
+
+
